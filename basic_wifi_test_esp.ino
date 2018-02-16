e57a817ad9bcf461e30db41fab5c7c3ff3903188
Kevin Kuwata <kevinkuwata@icloud.com>
2018-02-15 20:33:33 -0700

    working on a good interrupt routine

--- basic_wifi_test_esp.ino
+++ basic_wifi_test_esp.ino
@@ -1,21 +1,41 @@
 #include <ESP8266WiFi.h>
+
+#define INDICATOR_LED     D6 //GPIO 12 (ESP) d6 default
+#define RELAY_PIN         D0 //GPIO 16 (ESP)
+#define LED_PIN			  D2 //GPIO 02 (ESP) built in led
+#define BUTTON_PIN		  D5 //GPIO 14 (ESP)
+
  
-const char* ssid = "";//type your ssid
+const char* ssid = "android";//type your ssid
 const char* password = "kwatj0w1";//type your password
+
+volatile bool relay_onQ = false; // set the initial flag to not on.
+volatile bool change = false; //changed in the ISR.
  
-int ledPin = 2; // GPIO2 of ESP8266
 WiFiServer server(80);//Service Port
  
 void setup() {
-  Serial.begin(115200);
+    delay(100);
+	Serial.begin(115200);
     WiFi.mode(WIFI_STA); // SETS TO STATION MODE!
 
-  delay(10);
- 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  pinMode(0, OUTPUT);
-  pinMode(0, HIGH);
+  delay(100);
+ //LED
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+ //RELAY
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(RELAY_PIN, HIGH);
+ //INDICATOR_LED
+  pinMode(INDICATOR_LED, OUTPUT);
+  digitalWrite(INDICATOR_LED, HIGH);
+ // BUTTON PIN
+  pinMode(BUTTON_PIN, INPUT_PULLUP); //JUST IN CASE, BUT DONE IN HARDWARE
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggleISR, RISING); // make gpio14 an interrupt, trigger the function toggleISR, on any change
+  
+  
+    delay(100); //WIFI
+
    
   // Connect to WiFi network
   Serial.println();
@@ -24,10 +44,12 @@ void setup() {
   Serial.println(ssid);
    
   WiFi.begin(ssid, password);
-   
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(200);
     Serial.print(".");
+		ESP.wdtFeed();
+        delay(200);
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -45,6 +67,17 @@ void setup() {
 }
  
 void loop() {
+	
+	if(change == true){
+		digitalWrite(RELAY_PIN, HIGH);
+		delay(1000);
+		digitalWrite(RELAY_PIN, LOW);
+		change = false;
+	}
+	
+	
+	//feed wdt
+	ESP.wdtFeed();
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
@@ -52,32 +85,35 @@ void loop() {
   }
    
   // Wait until the client sends some data
-  Serial.println("new client");
+ // Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
    
   // Read the first line of the request
   String request = client.readStringUntil('\r');
-  Serial.println(request);
+//  Serial.println(request);
   client.flush();
    
   // Match the request
  
   int value = LOW;
   if (request.indexOf("/LED=ON") != -1) {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LED_PIN, LOW);
     value = LOW;
-    digitalWrite(0, LOW);
+    digitalWrite(RELAY_PIN, LOW);
+	digitalWrite(INDICATOR_LED, LOW);
   } 
   if (request.indexOf("/LED=OFF") != -1){
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     value = HIGH;
-        digitalWrite(0, HIGH);
+        digitalWrite(RELAY_PIN, HIGH);
+			digitalWrite(INDICATOR_LED, LOW);
+
   }
  
-  //Set ledPin according to the request
-  //digitalWrite(ledPin, value);
+  //Set LED_PIN according to the request
+  //digitalWrite(LED_PIN, value);
    
   // Return the response
   client.println("HTTP/1.1 200 OK");
@@ -94,11 +130,28 @@ void loop() {
     client.print("Off");
   }
   client.println("<br><br>");
-  client.println("Click <a href=\"/LED=ON\">here</a> turn the LED on pin 2 ON<br>");
-  client.println("Click <a href=\"/LED=OFF\">here turn the LED on pin 2 OFF<br>");
+  client.println("Click <a href=\"/LED=OFF\">here</a> turn the LED on pin 2 ON<br>");
+  client.println("Click <a href=\"/LED=ON\">here</a> turn the LED on pin 2 OFF<br>");
   client.println("</html>");
  
   delay(1);
-  Serial.println("Client disconnected");
-  Serial.println("");
+ // Serial.println("Client disconnected");
+  //Serial.println("");
+}
+
+
+
+/*
+==================================================
+==================================================
+			ISR for toggling OUTLET 
+==================================================
+==================================================
+	@brief: triggers whenever there is a change of the button
+	returns nothing
+	hardware debeounce in place. 
+*/
+void toggleISR(){
+Serial.println("triggered");
+change = true;
 }
